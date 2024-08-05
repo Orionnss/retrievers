ca45084036a6674953e4f99524a24b0e4d348f97
Cyrille Siouffi-Degraeve <cyrille.siouffi@protonmail.com>
2024-08-05 22:53:05 +0200

    test

--- src/tests/retrievers/test_CrossAttentionRetriever.py
+++ src/tests/retrievers/test_CrossAttentionRetriever.py
@@ -3,13 +3,13 @@ from ...retriever_transformers.retrievers.CrossAttentionRetriever import CrossAt
 retriever = CrossAttentionRetriever("bert-base-uncased", seed=42)
 
 def test_CrossAttentionRetriever_fit():
-    queries = ["Hello, my dog is cute", "Hello, my cat is cute"]
-    documents = ["Dogs are the best animals ", "Cats are usually ferocious and independent"]
+    queries = ["Words to trigger damages", "The sea is blue"]
+    documents = ["Words to trigger damages ", "The sea is blue"]
     losses = []
     loss_callback = lambda loss: losses.append(loss)
     num_epochs = 2
     batch_size = 2
-    args = CrossAttentionRetrieverTrainingArguments(batch_size=batch_size, shuffle=False, epochs=num_epochs, step_callback=loss_callback, learning_rate=1e-10)
+    args = CrossAttentionRetrieverTrainingArguments(batch_size=batch_size, shuffle=False, epochs=num_epochs, step_callback=loss_callback, learning_rate=1e-5)
     retriever.fit(queries, documents, args)
     print(losses)
     print(losses[0], losses[-1])
