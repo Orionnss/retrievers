b2c3b333f6a27975170c2d6f4cf3b9cd4e845626
Cyrille Siouffi-Degraeve <cyrille.siouffi@protonmail.com>
2024-08-07 12:07:45 +0200

    .

--- src/retriever_transformers/models/CrossAttentionDistancePredictor.py
+++ src/retriever_transformers/models/CrossAttentionDistancePredictor.py
@@ -1,6 +1,7 @@
-from torch.nn import Module, MultiheadAttention, Linear, Sigmoid
+from torch.nn import Module, MultiheadAttention, Linear, Sigmoid, ReLU
 from transformers import BatchEncoding, AutoModel
 from transformers.models.bert import BertModel
+from transformers.modeling_outputs import BaseModelOutputWithPoolingAndCrossAttentions
 import torch
 
 class CrossAttentionDistancePredictor(Module):
@@ -8,17 +9,21 @@ class CrossAttentionDistancePredictor(Module):
         super().__init__()
         if seed is not None:
             torch.manual_seed(seed)
-        self.query_model = AutoModel.from_pretrained(bert_checkpoint)
-        self.answer_model = AutoModel.from_pretrained(bert_checkpoint)
+        self.query_model: BertModel = AutoModel.from_pretrained(bert_checkpoint)
+        self.answer_model: BertModel = AutoModel.from_pretrained(bert_checkpoint)
         self.cross_attention = MultiheadAttention(embed_dim=768, num_heads=8)
-        sequence_length = 512
-        self.linear =  Linear(768*sequence_length, 1)
+        #sequence_length = 512
+        self.linear =  Linear(768, 32)
+        self.relu = ReLU()
+        self.linear2 = Linear(32, 1)
         self.sigmoid = Sigmoid()
 
 
     def forward(self, query_batch: BatchEncoding, answer_batch: BatchEncoding):
-        query_output = self.query_model(**query_batch, output_hidden_states=True, return_dict=True)
-        answer_output = self.answer_model(**answer_batch, output_hidden_states=True, return_dict=True)
-        query_attn, _ = self.cross_attention(query_output[0], answer_output[0], answer_output[0])
-        return self.sigmoid(self.linear(query_attn.flatten(start_dim=1))).view(query_batch["input_ids"].shape[0], -1)
+        query_output: BaseModelOutputWithPoolingAndCrossAttentions = self.query_model(**query_batch, output_hidden_states=True, return_dict=True)
+        answer_output: BaseModelOutputWithPoolingAndCrossAttentions = self.answer_model(**answer_batch, output_hidden_states=True, return_dict=True)
+        print(query_output.last_hidden_state.shape)
+        print(answer_output.last_hidden_state.shape)
+        query_attn, _ = self.cross_attention(query_output.last_hidden_state, answer_output.last_hidden_state, answer_output.last_hidden_state)
+        return self.sigmoid(self.linear2(self.relu(self.linear(query_attn[:,0])))).view(-1)
     
--- src/tests/models/test_CrossAttentionDistancePredictor.py
+++ src/tests/models/test_CrossAttentionDistancePredictor.py
@@ -10,7 +10,7 @@ def test_SimpleDistancePrediction():
     inputs_answers = tokenizer(["Hello, my cat is cute"], return_tensors="pt", padding="max_length", max_length=512)
     outputs = model(inputs_questions, inputs_answers)
     assert outputs is not None
-    assert outputs.shape == torch.Size([1, 1])
+    assert outputs.shape == torch.Size([1])
     assert outputs[0] > 0 and outputs[0] < 1
 
 def test_SimpleDistancePrediction_multiple():
@@ -18,7 +18,7 @@ def test_SimpleDistancePrediction_multiple():
     inputs_answers = tokenizer(["Hello, my cat is cute", "Hello, my dog is cute"], return_tensors="pt", padding="max_length", max_length=512)
     outputs = model(inputs_questions, inputs_answers)
     assert outputs is not None
-    assert outputs.shape == torch.Size([2, 1])
+    assert outputs.shape == torch.Size([2])
     assert outputs[0] > 0 and outputs[0] < 1
     assert outputs[1] > 0 and outputs[1] < 1
     assert outputs[0] != outputs[1]
